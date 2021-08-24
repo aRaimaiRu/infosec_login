0caf1425db4a43aec579d4a2308f81f6edf60716
= <=>
2021-08-24 18:37:44 +0700

    design text button backgroud

--- src/components/page/Login/Content.js
+++ src/components/page/Login/Content.js
@@ -1,45 +1,43 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import md5 from "md5";
 import PropTypes from "prop-types";
-import {valEmail, valPassword } from "../../../utils/validate"
+import { valEmail, valPassword } from "../../../utils/validate"
 import "./Content.css";
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
 
 const saltRounds = 10;
 function LoginContent(props) {
-  const [data,setData] = useState({
-    email:"",
-    password:""
+  const [data, setData] = useState({
+    email: "",
+    password: ""
   })
-  const [errortxt ,setErrortxt] = useState("");
+  const [errortxt, setErrortxt] = useState("");
 
-  const handleChange = (type,value)=>{
+  const handleChange = (type, value) => {
     console.log(data);
     setData({
       ...data,
-      [type]:value
+      [type]: value
     }
-      
+
     )
 
   }
   const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
-  const summit = ()=>{
-    
-    if(!valEmail(data.email)) {
-      setErrortxt("Invalid Email or Password!"); 
+  const summit = () => {
+
+    if (!valEmail(data.email)) {
+      setErrortxt("Invalid Email or Password!");
       return
     };
-    if(!valPassword(data.password)){
-      setErrortxt("Invalid Email or Password!"); 
+    if (!valPassword(data.password)) {
+      setErrortxt("Invalid Email or Password!");
       return
     };
     setErrortxt("");
     let hash = md5(data.password);
     console.log(hash);
-    
+
 
   }
   return (
@@ -49,76 +47,74 @@ function LoginContent(props) {
           class="loginform bg-white size-lg"
           style={{
             justifyContent: "space-evenly",
-            padding: "50px",
+            padding: "10px",
             backgroundColor: "white",
           }}
         >
-        <center>
-          <div class="mb-4">
-          
-           <p>Email</p>
-           <label for="username"></label>
-            
-            <input
-              id="username"
-              type="text"
-              placeholder="Email"
-              value={data.email}
-              onChange={(e)=>handleChange("email",e.target.value)}
-            />
-          </div>
-         
-          <div class="mb-6">
-            <label for="password">
-            <p>Password</p>
-            </label>
-            <input
-              id="password"
-              type="password"
-              placeholder="******************"
-              value={data.password}
-              onChange={(e)=>handleChange("password",e.target.value)}
-            />
-            <p class="text-red text-xs colorRed">{errortxt}</p>
-          </div>
-          <div className="flex items-center justify-between">
-            <button type="button"onClick={summit}
-            >
-              Sign In
-            </button>
-            <br></br>
-            <br></br>
-            <button
-              
-              type="button"
-              onClick={summit}
-            >
-              register
-            </button>
-            <br></br>
-            <a
-              
-              href="#"
-            >
-              Forgot Password?
-            </a>
-        
-           
-        </div>
-      </center>
-        <ul className="circle">
+          <center>
+            <div>
+              <label for="username"></label>
+              <p>Email</p>
+              <input class="form-control mr-sm-2"
+                id="username"
+                type="text"
+                placeholder="Email"
+                value={data.email}
+                onChange={(e) => handleChange("email", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label for="password">
+              </label>
+              <p>Password</p>
+              <input class="form-control mr-sm-2"
+                id="password"
+                type="password"
+                placeholder="******************"
+                value={data.password}
+                onChange={(e) => handleChange("password", e.target.value)}
+              />
+              <p class="text-red text-xs colorRed">{errortxt}</p>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <button class="btn btn-primary btn-md mx-sm-2"
+                type="button" onClick={summit}
+              >
+                Sign In
+              </button>
+
+              <button class="btn btn-outline-primary my-2 my-sm-0 mx-sm-2"
+                type="button"
+                onClick={summit}
+              >
+                register
+              </button>
+              <br></br>
+              <a
+
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+
+          </center>
+          <hr></hr>
+          <ul className="circle">
             <li>Email validation</li>
             <li>Password Length more {'>'}8  {'<=64'}</li>
             <li>Must have Email and Password</li>
             <li>secret password</li>
             <li>Strong password</li>
-      </ul>
+          </ul>
         </form>
       </div>
 
 
     </div>
-    
+
   );
 }
 
--- src/components/page/Register/content.js
+++ src/components/page/Register/content.js
@@ -1,42 +1,42 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./Content.css";
 import md5 from "md5";
-import {valEmail, valPassword } from "../../../utils/validate"
+import { valEmail, valPassword } from "../../../utils/validate"
 function RegisterContent(props) {
-  const [data,setData] = useState({
-    email:"",
-    password:"",
-    name:"",
-    surname:""
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    surname: ""
   })
-  const [errortxt ,setErrortxt] = useState("");
+  const [errortxt, setErrortxt] = useState("");
 
-  const handleChange = (type,value)=>{
+  const handleChange = (type, value) => {
     console.log(data);
     setData({
       ...data,
-      [type]:value
+      [type]: value
     }
-      
+
     )
 
   }
   const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
 
-  const summit = ()=>{
-    if(!valEmail(data.email)) {
-      setErrortxt("Invalid Email or Password!"); 
+  const summit = () => {
+    if (!valEmail(data.email)) {
+      setErrortxt("Invalid Email or Password!");
       return
     };
-    if(!valPassword(data.password)){
-      setErrortxt("Invalid Email or Password!"); 
+    if (!valPassword(data.password)) {
+      setErrortxt("Invalid Email or Password!");
       return
     };
     setErrortxt("");
     let hash = md5(data.password);
     console.log(hash);
-    
+
 
   }
   return (
@@ -48,93 +48,103 @@ function RegisterContent(props) {
             justifyContent: "space-evenly",
             padding: "50px",
             backgroundColor: "white",
-            overflow:"auto"
+            overflow: "auto"
           }}
         ><center>
-          <div class="mb-4">
-          
-            <label
-              class="block text-grey-darker text-sm font-bold mb-2"
-              for="username"
-            >
-              <p>Email</p>
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-              id="username"
-              type="text"
-              placeholder="Email"
-              value={data.email}
-              onChange={(e)=>handleChange("email",e.target.value)}
-            />
-          </div>
-          <div class="mb-4">
-            <label
-              class="block text-grey-darker text-sm font-bold mb-2"
-              for="password"
-            >
-            <p>Password</p>
-            </label>
-            <input
-              class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
-              id="password"
-              type="password"
-              placeholder="******************"
-              value={data.password}
-              onChange={(e)=>handleChange("password",e.target.value)}
-            />
-            
-          </div>
-          <div class="mb-4">
-            <label
-              class="block text-grey-darker text-sm font-bold mb-2"
-              for="password"
-            >
-              <p>Name</p>
-            </label>
-            <input
-              class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
-              id="Name"
-              type="Text"
-              placeholder="Name"
-              value={data.name}
-              onChange={(e)=>handleChange("name",e.target.value)}
-            />
-            
-          </div>
-          <div class="mb-4">
-            <label
-              class="block text-grey-darker text-sm font-bold mb-2"
-              for="password"
-            >
-              <p>Surname</p>
-            </label>
-            <input
-              class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
-              id="Surname"
-              type="Text"
-              placeholder="Surname"
-              value={data.surname}
-              onChange={(e)=>handleChange("surname",e.target.value)}
-            />
-            <br></br>
-            <br></br>
-            <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={summit}
-            >
-              Register
-            </button>
-            <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={summit}
-            >
-              Sign In
-            </button>
-          </div>
+            <form class="form-inline">
+              <div class="form-group row mb-4">
+                <label
+                  class="block text-grey-darker text-sm font-bold col-sm-2"
+                  for="username"
+                >
+                  Email
+                </label>
+                <div class="col-sm-10">
+                  <input
+                    class="form-control mr-sm-2"
+                    id="username"
+                    type="text"
+                    placeholder="Email"
+                    value={data.email}
+                    onChange={(e) => handleChange("email", e.target.value)}
+                  /></div>
+              </div>
+            </form>
+            <form class="form-inline">
+              <div class="form-group row mb-4">
+                <label
+                  class="block text-grey-darker text-sm font-bold col-sm-2"
+                  for="password"
+                >
+                  Password
+                </label>
+                <div class="col-sm-9">
+                  <input
+                    class="form-control mr-sm-2"
+                    id="password"
+                    type="password"
+                    placeholder="******************"
+                    value={data.password}
+                    onChange={(e) => handleChange("password", e.target.value)}
+                  /></div>
+              </div>
+            </form>
+            <form class="form-inline">
+              <div class="form-group row mb-4">
+                <label
+                  class="block text-grey-darker text-sm font-bold col-sm-2"
+                  for="password"
+                >
+                  Name
+                </label>
+                <div class="col-sm-10">
+                  <input
+                    class="form-control mr-sm-2"
+                    id="Name"
+                    type="Text"
+                    placeholder="Name"
+                    value={data.name}
+                    onChange={(e) => handleChange("name", e.target.value)}
+                  /></div>
+              </div>
+            </form>
+            <form class="form-inline">
+              <div class="form-group row mb-4">
+                <label
+                  class="block text-grey-darker text-sm font-bold col-sm-2"
+                  for="password"
+                >
+                  Surname
+                </label>
+                <div class="col-sm-9">
+                  <input
+                    class="form-control mr-sm-2"
+                    id="Surname"
+                    type="Text"
+                    placeholder="Surname"
+                    value={data.surname}
+                    onChange={(e) => handleChange("surname", e.target.value)}
+                  /></div>
+              </div>
+            </form>
+            <div className="flex items-center justify-between">
+              <button
+                class="btn btn-primary btn-md mx-sm-2"
+                type="button"
+                onClick={summit}
+              >
+                Register
+              </button>
+              <button
+                class="btn btn-outline-primary my-2 my-sm-0 mx-sm-2"
+                type="button"
+                onClick={summit}
+              >
+                Sign In
+              </button>
+            </div>
           </center>
+          <hr></hr>
           <ul className="circle">
             <li>Email validation</li>
             <li>Password Length more {'>'}8  {'<=64'}</li>
@@ -143,14 +153,7 @@ function RegisterContent(props) {
             <li>Strong password</li>
           </ul>
         </form>
-
       </div>
-      
-        
-          
-
-        
-      
     </div>
   );
 }
