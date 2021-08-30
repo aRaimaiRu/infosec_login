a7b76f457908830820967ad19a920e3373d068ab
Kittipat <first1801@hotmail.com>
2021-08-30 18:20:45 +0700

    only alphabet in name and surname

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,5 @@
 const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+const namePattern = /^[a-zA-Z]*$/g;
 const valEmail = (email)=>{
     //check username length
     if(email.length <1) return false;
@@ -11,9 +12,11 @@ const valPassword = (password)=>{
     return true
 }
 const valName = (name)=>{
+    if (!namePattern.test(name)) return false;
     if(name.length <1) return false;
 }
 const valSurname = (surname)=>{
+    if (!namePattern.test(surname)) return false;
     if(surname.length <1) return false;
 }
   
