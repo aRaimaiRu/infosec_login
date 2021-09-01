6e9a41c593dca904917d1972495f738de91be252
Kittipat <first1801@hotmail.com>
2021-09-01 19:00:30 +0700

    fix validate bug

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,5 +1,5 @@
 const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-const namePattern = /^[a-zA-Z]*$/g;
+const namePattern = /^[a-zA-Z]*$/;
 const valEmail = (email)=>{
     //check username length
     if(email.length <1) return false;
@@ -14,10 +14,13 @@ const valPassword = (password)=>{
 const valName = (name)=>{
     if (!namePattern.test(name)) return false;
     if(name.length <1) return false;
+    return true;
 }
 const valSurname = (surname)=>{
     if (!namePattern.test(surname)) return false;
     if(surname.length <1) return false;
+    return true;
 }
+
   
   export { valEmail, valPassword,valName, valSurname};
