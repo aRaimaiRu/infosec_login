78bfe1e4bdf21105dc56806d435c9f21b87c2188
Kittipat <first1801@hotmail.com>
2021-09-22 00:40:53 +0700

    set dotenv for deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+require('dotenv').config()
 import { useRecoilState,RecoilRoot } from 'recoil';
 import './index.css';
 import App from './App';
