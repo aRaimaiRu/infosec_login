36f8ede22dfccd9e4e3223d3732e8b06280090dc
Kittipat <first1801@hotmail.com>
2021-10-19 18:09:08 +0700

    add login/logout to homePage

--- src/components/page/Home/content.js
+++ src/components/page/Home/content.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Productdetaillayout from '../../productdetaillayout';
 import homestyle from '../../../styles/HomePage.module.css';
 import searchstyle from '../../../styles/search.module.css';
 import EmblaCarousel from '../../EmblaCarousel';
+import { getOwnData, refreshToken, logout } from '../../../utils/userAPI';
+import { useHistory, Link } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { tokenState } from '../../../store';
 function HomeContent(props) {
   const SLIDE_COUNT = 2;
   const slides = Array.from(Array(SLIDE_COUNT).keys());
-  const userInfo = {
-    name: 'name',
-  };
+  let history = useHistory();
+  const [token, setToken] = useRecoilState(tokenState);
   return (
     <Productdetaillayout>
       <div className={homestyle.homenavbar}>
-        <div className={homestyle.divider}>
-          <h1>{userInfo.name ? userInfo.name : 'โปรไฟล์'}</h1>
-        </div>
         <div className={homestyle.divider}>
           <h1>เปิดร้าน</h1>
         </div>
         <div className={homestyle.divider}>
           <h1>รายการสินค้า</h1>
         </div>
-        <div className={homestyle.divider} style={{ border: 'none' }}>
-          <h1>{userInfo ? 'Logout' : 'Login'}</h1>
-        </div>
+        {/* login   /   logout */}
+        {token.name === undefined ? (
+          <div
+            className={homestyle.divider}
+            style={{ border: 'none' }}
+            onClick={() => {
+              history.push('/login');
+            }}
+          >
+            <h1>login</h1>
+          </div>
+        ) : (
+          <div
+            className={homestyle.divider}
+            style={{ border: 'none' }}
+            onClick={() => logout()}
+          >
+            <h1>logout</h1>
+          </div>
+        )}
+        {/* END  */}
       </div>
       <h1 style={{ alignSelf: 'start' }}>ข่าวสาร</h1>
       <EmblaCarousel slides={slides}></EmblaCarousel>
