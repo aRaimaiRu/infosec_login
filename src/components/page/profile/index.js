16fd6ffa374d3141dce3cd3d515df4bd5757935e
22p26c0377 <pawut.j@gmail.com>
2021-10-25 23:56:21 +0800

    Add Profile Complate

--- src/components/page/profile/index.js
+++ src/components/page/profile/index.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import Layout from "../../productdetaillayout";
 import "./profile.css";
+
+const profileData = {
+  name: "ยินดี จ่ายเงิน",
+  address: "อะไรก็ไม่รู้ สมมุติว่ายาวมาก ยาวมากกกกกกกกก 112112112",
+  tel: "0123456789",
+};
 const Profile = (props) => {
   return (
     <Layout>
-      <div style={{ backgroundColor: "skyblue", width: "100%" }}>
+      <div style={{ width: "100%" }} className="profile p-3">
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-sm-4">
             <h3>โปรไฟล์</h3>
           </div>
-          <div className="col-md-4"></div>
-          <div className="col-md-4 text-right">
+          <div className="col-sm-4"></div>
+          <div className="col-sm-4 text-right">
             <h3>ย้อนกลับ</h3>
           </div>
         </div>
@@ -27,7 +33,33 @@ const Profile = (props) => {
             <button class="btn-img">Take</button>
           </div>
         </div>
-        <div className="row"></div>
+        <div className="row">
+          <div className="col">
+            <h3>รายละเอียดโปรไฟล์</h3>
+            <hr />
+            <h4>ชื่อผู้ใช้ : {profileData.name}</h4>
+            <h4>ที่อยู่ : {profileData.address}</h4>
+            <h4>เบอร์โทร : {profileData.tel}</h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <h3>สินค้าที่ดูล่าสุด</h3>
+          </div>
+        </div>
+        <div className="row">
+          <img
+            src="/images/image_test.jpg"
+            className="img-product m-1"
+            alt="product"
+          />
+
+          <img
+            src="/images/image_test.jpg"
+            className="img-product m-1"
+            alt="product"
+          />
+        </div>
       </div>
     </Layout>
   );
