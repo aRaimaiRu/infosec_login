637f0f3141d460c904f5aec57fe19de3d2251df0
Kittipat <first1801@hotmail.com>
2021-11-05 13:02:57 +0700

    set orderwaranty in order page

--- src/components/page/order/index.js
+++ src/components/page/order/index.js
@@ -24,6 +24,7 @@ const profileData = {
   tel: '0123456789',
 };
 const Order = (props) => {
+  const [isOrderWarrant, setOrderWarrant] = useState(false);
   let history = useHistory();
   let { id } = useParams();
   const [token, setToken] = useRecoilState(tokenState);
@@ -49,6 +50,40 @@ const Order = (props) => {
       setProduct(data);
     }
   }, []);
+
+  const orderwarrant = (
+    <>
+      {' '}
+      <div className="row pt-3">
+        <div className="col">
+          <h4 className="ascend">เงื่อนไขในการรับประกันสินค้า</h4>
+          <h4>
+            A long time ago, a group of special girls known as ‘mahou shoujos’
+            saved the world after a fierce battle where blood and tears were
+            shed and prayers were made. It was an overly common battle tale.
+            However, no one thanked or praised them for their victory. In fact,
+            no one even knew them at all. Even so, the future of humanity was
+            protected and it came to a happy end.
+          </h4>
+        </div>
+      </div>
+      <hr />
+      <div className="row ">
+        <div className="col pt-30">
+          <div className="d-flex justify-content-around ">
+            <button className="btn btn-danger button-custom">
+              <h4>ยกเลิก</h4>
+            </button>
+            <Link to={`/payment/${id}`}>
+              <button className="btn btn-success button-custom">
+                <h4>ยืนยัน</h4>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
   return (
     <Layout>
       <div style={{ width: '100%' }} className="p-3 mt-3 order">
@@ -75,44 +110,60 @@ const Order = (props) => {
           </div>
         </div>
         <hr />
-        <div className="row pt-3">
-          <div className="col">
-            <h4 className="ascend">ที่อยู่สำหรับจัดส่ง</h4>
-            <h4>
-              {product.user.firstName} {product.user.lastName}{' '}
-              {product.user.tel}
-            </h4>
-            <h4>{product.address}</h4>
-          </div>
-        </div>
-        <hr />
-        <div className="row pt-3">
-          <div className="col">
-            <h4 className="ascend">รายละเอียดคำสั่งซื้อ</h4>
-            <h4>รวมการสั่งซื้อ: {product.product.price} บาท</h4>
-            <h4>ค่าส่ง: {product.product.price} บาท</h4>
-            <h4>ยอดเงินที่กำหนด: {product.product.price} บาท</h4>
-          </div>
-        </div>
-        <div className="row ">
-          <div className="col pt-30">
-            <div className="d-flex justify-content-around ">
-              <button
-                className="btn btn-danger button-custom"
-                onClick={() => {
-                  history.goBack();
-                }}
-              >
-                <h4>ยกเลิก</h4>
-              </button>
-              <Link to={`/payment/${id}`}>
-                <button className="btn btn-success button-custom">
-                  <h4>ยืนยัน</h4>
-                </button>
-              </Link>
+        {isOrderWarrant && orderwarrant}
+        {!isOrderWarrant && (
+          <>
+            <div className="row pt-3">
+              <div className="col">
+                <h4 className="ascend">ที่อยู่สำหรับจัดส่ง</h4>
+                <h4>
+                  {product.user.firstName} {product.user.lastName}{' '}
+                  {product.user.tel}
+                </h4>
+                <h4>{product.address}</h4>
+              </div>
             </div>
-          </div>
-        </div>
+            <hr />
+            <div className="row pt-3">
+              <div className="col">
+                <h4 className="ascend">รายละเอียดคำสั่งซื้อ</h4>
+                <h4>รวมการสั่งซื้อ: {product.product.price} บาท</h4>
+                <h4>ค่าส่ง: {product.product.price} บาท</h4>
+                <h4>ยอดเงินที่กำหนด: {product.product.price} บาท</h4>
+              </div>
+            </div>
+            <div className="row ">
+              <div className="col pt-30">
+                <div className="d-flex justify-content-around ">
+                  <button
+                    className="btn btn-danger button-custom"
+                    onClick={() => {
+                      history.goBack();
+                    }}
+                  >
+                    <h4>ยกเลิก</h4>
+                  </button>
+                  {/* <Link to={`/payment/${id}`}> */}
+                  <button
+                    className="btn btn-success button-custom"
+                    onClick={() => {
+                      console.log(isOrderWarrant);
+                      if (isOrderWarrant) {
+                        console.log('push history');
+                        history.push(`/payment/${id}`);
+                      } else {
+                        setOrderWarrant(true);
+                      }
+                    }}
+                  >
+                    <h4>ยืนยัน</h4>
+                  </button>
+                  {/* </Link> */}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </Layout>
   );
