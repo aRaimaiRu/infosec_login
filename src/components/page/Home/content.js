78a7178e5c7cb84997a98657864ddfeccee886e1
22p26c0377 <pawut.j@gmail.com>
2021-11-07 13:06:30 +0800

    Fix image Squre

--- src/components/page/Home/content.js
+++ src/components/page/Home/content.js
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import Productdetaillayout from '../../productdetaillayout';
-import homestyle from '../../../styles/HomePage.module.css';
-import searchstyle from '../../../styles/search.module.css';
-import EmblaCarousel from '../../EmblaCarousel';
-import { getOwnData, refreshToken, logout } from '../../../utils/userAPI';
-import { useHistory, Link } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { tokenState } from '../../../store';
-import SearchBar from '../../searchbar';
+import React, { useState, useEffect } from "react";
+import Productdetaillayout from "../../productdetaillayout";
+import homestyle from "../../../styles/HomePage.module.css";
+import searchstyle from "../../../styles/search.module.css";
+import EmblaCarousel from "../../EmblaCarousel";
+import { getOwnData, refreshToken, logout } from "../../../utils/userAPI";
+import { useHistory, Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { tokenState } from "../../../store";
+import SearchBar from "../../searchbar";
 function HomeContent(props) {
   const SLIDE_COUNT = 2;
   const slides = Array.from(Array(SLIDE_COUNT).keys());
   let history = useHistory();
   const [token, setToken] = useRecoilState(tokenState);
-  console.log('hometoken =', token);
+  console.log("hometoken =", token);
 
   const showOpenShopOrOwnShop =
-    (token.Role === 'Customer' && (
+    (token.Role === "Customer" && (
       <div
         className={homestyle.divider}
         onClick={() => {
-          history.push('/ShopRegister');
+          history.push("/ShopRegister");
         }}
       >
         <h1>เปิดร้าน</h1>
       </div>
     )) ||
     // if Role ShopOwner
-    (token.Role === 'ShopOwner' && (
+    (token.Role === "ShopOwner" && (
       <div
         className={homestyle.divider}
         onClick={() => {
-          history.push('/OwnShop');
+          history.push("/OwnShop");
         }}
       >
         <h1>ดูร้าน</h1>
@@ -40,12 +40,16 @@ function HomeContent(props) {
 
   const showProduct = [
     {
-      url: '/product/1',
-      img: '/images/SneakerRed.jpg',
+      url: "/product/1",
+      img: "/images/SneakerRed.jpg",
     },
     {
-      url: '/product/2',
-      img: '/images/SneakerRed.jpg',
+      url: "/product/2",
+      img: "/images/SneakerRed.jpg",
+    },
+    {
+      url: "/product/2",
+      img: "/images/SneakerRed.jpg",
     },
   ];
   return (
@@ -69,9 +73,9 @@ function HomeContent(props) {
         {token.firstName === undefined ? (
           <div
             className={homestyle.divider}
-            style={{ border: 'none' }}
+            style={{ border: "none" }}
             onClick={() => {
-              history.push('/login');
+              history.push("/login");
             }}
           >
             <h1>login</h1>
@@ -79,7 +83,7 @@ function HomeContent(props) {
         ) : (
           <div
             className={homestyle.divider}
-            style={{ border: 'none' }}
+            style={{ border: "none" }}
             onClick={() => logout(setToken)}
           >
             <h1>logout</h1>
@@ -87,7 +91,7 @@ function HomeContent(props) {
         )}
         {/* END  */}
       </div>
-      <h1 style={{ alignSelf: 'start' }}>ข่าวสาร</h1>
+      <h1 style={{ alignSelf: "start" }}>ข่าวสาร</h1>
       <EmblaCarousel slides={slides}></EmblaCarousel>
       <h1>รายการสินค้า</h1>
       <div className={searchstyle.searchresultcontainer}>
