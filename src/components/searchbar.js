bc7691c4f4982799c99f7bd9b7537aa89baf679d
Kittipat <first1801@hotmail.com>
2021-11-07 11:54:51 +0700

    broken searchnar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,24 +1,40 @@
-import { Link } from 'react-router-dom';
 import loginpage from '../styles/loginpage.module.css';
+import { Controller, useForm } from 'react-hook-form';
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+  Link,
+  useHistory,
+} from 'react-router-dom';
 export default function SearchBar({ children }) {
+  let history = useHistory();
+  const { register, handleSubmit } = useForm();
+  const submit = (data) => {
+    history.push('search?' + data.search);
+  };
   return (
-    <div
+    <form
       className={loginpage.loginwithcontainer}
       style={{ marginTop: '30px', width: '100%' }}
+      onSubmit={handleSubmit(submit)}
     >
       <input
         type="text"
         className={loginpage.inputwidth100}
         style={{ marginRight: '20px' }}
+        {...register('search')}
       />
-      <Link href="/search/1">
+      <button type="submit" value="submit">
         <img
           src="/images/searchimg.png"
           width={50}
           height={50}
           color="red"
         ></img>
-      </Link>
-    </div>
+      </button>
+    </form>
   );
 }
