0d1ef8504d7efa616d89770d4a4cc671ecf49a24
22p26c0377 <pawut.j@gmail.com>
2021-11-07 15:05:00 +0800

    Footer Complate

--- src/components/page/Home/HomeFooter.js
+++ src/components/page/Home/HomeFooter.js
@@ -5,8 +5,11 @@ function HomeFooter(props) {
   return (
     <div className="homefooter ">
       <div className="row align-items-center h-100 ">
-        <div className="col text-center">
+        <div className="col d-flex justify-content-around">
           <button className="btn btn-custom">อนุมัติเปิดร้าน</button>
+          <button className="btn btn-custom-2 bg-primary">
+            รายชื่อร้านค้า
+          </button>
         </div>
       </div>
     </div>
